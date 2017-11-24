8d333fbb1c3d2d8b724f7eeed7d0dbcd0ad5d6e2
sergio.roldan <sergioroldan.macia@gmail.com>
2017-11-24 21:04:53 +0100

    Refractor

--- server.js
+++ server.js
@@ -1,11 +1,14 @@
 var express = require('express');
+var bodyParser = require('body-parser');
 var _ = require('underscore');
 
 var app = express();
 var PORT = process.env.PORT || 8080;
 
-
+var todoNextId = 1;
 var todos = [];
+
+app.use(bodyParser.json());
 
 app.get('/', function(req, res){
   res.send('Todo API root');
@@ -16,13 +19,33 @@ app.get('/todos', function(req, res) {
 });
 
 app.get('/todos/:id', function(req, res){
-  var todoId = req.params.id;
-  var matchedTodo;
+  var todoId = parseInt(req.params.id, 10);
+  var matchedTodo = _.findWhere(todos, {id: todoId});
 
-  if (matchedTodo)
-    res.json(matchedTodo)
-  else
+  console.log(matchedTodo);
+
+  if (matchedTodo) {
+    res.json(matchedTodo);
+  }
+  else {
     res.status(404).send();
+  }
+});
+
+app.post('/todos', function(req, res){
+  var body = _.pick(req.body, 'description', 'completed');
+
+  if (!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length === 0) {
+    return res.status(400).send();
+  }
+
+
+  body.description = body.description.trim();
+
+  body.id = todoNextId++;
+  todos.push(body);
+
+  res.json(body);
 });
 
 app.listen(PORT, function(){
