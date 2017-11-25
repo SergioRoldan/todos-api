a52e20ecf1c081a97984c6f23427e62caa0a1c45
sergio.roldan <sergioroldan.macia@gmail.com>
2017-11-25 15:53:25 +0100

    Lower Case

--- server.js
+++ server.js
@@ -25,6 +25,12 @@ app.get('/todos', function(req, res) {
       filteredTodos = _.where(filteredTodos, {completed: false});
   }
 
+  if (queryParameters.hasOwnProperty('q') && queryParameters.q.trim().length > 0) {
+      filteredTodos = _.filter(filteredTodos, function(todo) {
+          return todo.description.toLowerCase().indexOf(queryParameters.q.toLowerCase()) > -1;
+      });
+  }
+
 
   res.json(filteredTodos);
 });
