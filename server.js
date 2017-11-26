546ba13eab83fcbbfef1bcda48d9e6bef5e80903
sergio.roldan <sergioroldan.macia@gmail.com>
2017-11-26 11:50:59 +0100

    Fix fix

--- server.js
+++ server.js
@@ -205,19 +205,18 @@ app.post('/users', function(req, res) {
 
 });
 
-app.post('/users/login', function(req, res) {
-  var body = _.pick(req.body, 'email', 'password');
+app.post('/users/login', function (req, res) {
+	var body = _.pick(req.body, 'email', 'password');
 
-  db.user.authenticate(body).then(function (user) {
-    res.json(user.toPublicJSON());
-  }, function() {
-    res.status(401).send();
-  });
-
+	db.user.authenticate(body).then(function (user) {
+		res.json(user.toPublicJSON());
+	}, function () {
+		res.status(401).send();
+	});
 });
 
 db.sequelize.sync({force: true}).then(function() {
-  app.listen(PORT, function(){
-     console.log("Express listening on the port " + PORT + "!");
-  });
+	app.listen(PORT, function() {
+		console.log('Express listening on port ' + PORT + '!');
+	});
 });
